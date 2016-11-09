b41eba86ab4c4c92928551e50e3f75ea7b88df6f
AntoineBarroux <antoine.barroux@gmail.com>
2016-11-09 13:09:31 +0100

    Adding native calcul

--- SampleAngular/HomeViewController.swift
+++ SampleAngular/HomeViewController.swift
@@ -17,8 +17,12 @@ class HomeViewController: CobaltViewController, CobaltDelegate {
         super.init(coder: coder)
         NSLog("passage dans init")
         self.setDelegate(self)
-        Cobalt.setResourcePath(Bundle.main.resourcePath! + "/common/")
+        Cobalt.setResourcePath(Bundle.main.resourcePath! + "/Samples-Angular2-Web/")
         initWithPage("index.html", andController: "default")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
     override func viewDidLoad() {
@@ -39,7 +43,21 @@ class HomeViewController: CobaltViewController, CobaltDelegate {
     //  MARK : DELEGATE
     
     func onUnhandledEvent(_ event: String, withData data: [AnyHashable : Any], andCallback callback: String) -> Bool{
-        
+        print("avant le if")
+        if (event == "nativeCalcul"){
+            NSLog("dans le if");
+            if let nb1:Int = data["nb1"] as? Int{
+                if let nb2:Int = data["nb2"] as? Int{
+                    let result:Int = nb1 + nb2
+                    let dataCallback:[AnyHashable:Any] = [
+                        AnyHashable("result"): result
+                    ]
+                    self.sendCallback(callback, withData: dataCallback as NSObject!)
+                    
+                    return true
+                }
+            }
+        }
         return false
     }
     
